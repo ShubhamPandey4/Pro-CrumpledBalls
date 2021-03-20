a09a64eba1212edf34ef2785bd670985e48885fe
Sheema Parwaz <Sheemaparwaz@gmail.com>
2021-03-20 20:26:35 +0530

    debugged

--- Paper.js
+++ Paper.js
@@ -2,25 +2,23 @@ class Paper{
       constructor(x,y,r){
           var options={
               restitution:0.3,
-              isStatic:false,
               friction:0.5,
               density:1.2
           }
       this.x=x;
       this.y=y;
       this.r=r;
-      this.body=Bodies.circle(this.x, this.y,this.r-20, options)
+      this.body=Bodies.circle(this.x, this.y,this.r, options)
       World.add(world, this.body);
       }
       display()
       {
           var paperpos=this.body.position;
-          push()
-          translate(paperpos.x, paperpos.y);
+         
           ellipseMode(RADIUS);
           strokeWeight(3);
           fill(255,0,255);
-          ellipse(0,0,this.r,this.r);
-          pop()
+          ellipse(paperpos.x,paperpos.y,this.r,this.r);
+         
       }
 }
